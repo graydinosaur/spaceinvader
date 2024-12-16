2af4e51561e325e2686b00889e580f7ae8cda265
graydinosaur <notahacker256@outlook.com>
2024-12-16 11:35:36 +0100

    created variables for the ship image

--- space invaders/src/SpaceInvaders.java
+++ space invaders/src/SpaceInvaders.java
@@ -1,10 +1,31 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 
 public class SpaceInvaders extends JPanel{
-    //size of panel
+    class Block {
+        int a;
+        int y;
+        int width;
+        int height;
+        Image img;
+        boolean alive = true // used for aliens
+        boolean used = false // used for bullets
+        
+        //constructor
+        Block(int x, int y, int width, int height, Iamge img) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.img = img;
+        }
+    }
+    //size of board
     int tileSize = 32;
     int rows = 16;
     int columns =16;
@@ -20,18 +41,30 @@ public class SpaceInvaders extends JPanel{
     //store in array
     ArrayList<Image> alienImArray;
 
+    //ship
+    int shipWidth = tileSize*2;
+    int shipHeight = tileSize;
+    int shipX = tileSize*columns/2 - tileSize;
+    int shipY = boardWeight - tileSize*2;
+
+
     // constructor
     SpaceInvaders() {
         setPreferredSize(new Dimension(boardWidth,boardHeight));
         setBackground(Color.BLACK);
 
+        // load images
         shipImg = new ImageIcon(getClass().getResource("./images/ship.png")).getImage();
         alienImg= new ImageIcon(getClass().getResource("./images/alien.png")).getImage();
         alienCyanImg = new ImageIcon(getClass().getResource("./images/alien-cyan.png")).getImage();
         alienMagentaImg = new ImageIcon(getClass().getResource("./images/alien-magenta.png")).getImage();
         alienYellowImg = new ImageIcon(getClass().getResource("./images/alien-yellow.png")).getImage();
 
-
+        alienImgArray = new ArrayList<Image>();
+        alienImgArray.add(alienImg);
+        alienImgArray.add(alienCyanImg);
+        alienImgArray.add(alienMagentaImg);
+        alienImgArray.add(alienYellowImg);
 
     }
 }
