e2ef6d97a619952dc2b90cf06c63e62d41636964
graydinosaur <notahacker256@outlook.com>
2024-12-16 10:53:32 +0100

    size of panel

---
+++ space invaders/src/SpaceInvaders.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+
+public class SpaceInvaders extends JPanel{
+    //size of panel
+    int tileSize = 32;
+    int rows = 16;
+    int columns =16;
+    int boardWidth = tileSize * columns;
+    int boardHeight = tileSize * rows;
+}
