76adfd2157771a1e9c58f87dbe1e4e58d320d0c0
graydinosaur <notahacker256@outlook.com>
2024-12-16 10:22:05 +0100

    add window variables

---
+++ space invaders/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int tileSize = 32;
+        int rows =16;
+        int columns=16;
+        int boardWidth = tileSIze * columns;
+        int boardHeight = tileSize * rows;
+    }
+}
