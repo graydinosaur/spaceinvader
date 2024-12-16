13264057ac4cc1b7c8137cba234579afe706fb8d
graydinosaur <notahacker256@outlook.com>
2024-12-16 10:42:00 +0100

    added frame

--- space invaders/src/App.java
+++ space invaders/src/App.java
@@ -1,9 +1,18 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         int tileSize = 32;
         int rows =16;
         int columns=16;
-        int boardWidth = tileSIze * columns;
+        int boardWidth = tileSize * columns;
         int boardHeight = tileSize * rows;
+
+        JFrame frame = new JFrame("Space invaders");
+        frame.setVisible(true);
+        frame.setSize(boardHeight,boardWidth);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
