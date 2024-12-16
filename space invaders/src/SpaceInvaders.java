21472499357e49c691563576b4258ce5293791e8
graydinosaur <notahacker256@outlook.com>
2024-12-16 12:02:18 +0100

    created arraylist to store images,adding images, creates constructor  for players spaceship, add graphics panel and draw the ship

--- space invaders/src/SpaceInvaders.java
+++ space invaders/src/SpaceInvaders.java
@@ -1,23 +1,23 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
-import javax.swing.*;
-
-
-public class SpaceInvaders extends JPanel{
+public class SpaceInvaders extends JPanel {
     class Block {
-        int a;
+        int x; // Corrected from 'a'
         int y;
         int width;
         int height;
         Image img;
-        boolean alive = true // used for aliens
-        boolean used = false // used for bullets
-        
-        //constructor
-        Block(int x, int y, int width, int height, Iamge img) {
+        boolean alive = true; // Added semicolon
+        boolean used = false; // Added semicolon
+
+        // Constructor
+        Block(int x, int y, int width, int height, Image img) {
             this.x = x;
             this.y = y;
             this.width = width;
@@ -25,46 +25,63 @@ public class SpaceInvaders extends JPanel{
             this.img = img;
         }
     }
-    //size of board
+
+    // Size of board
     int tileSize = 32;
     int rows = 16;
-    int columns =16;
+    int columns = 16;
     int boardWidth = tileSize * columns;
     int boardHeight = tileSize * rows;
 
-    //variables images
+    // Variables for images
     Image shipImg;
     Image alienImg;
     Image alienCyanImg;
     Image alienMagentaImg;
     Image alienYellowImg;
-    //store in array
-    ArrayList<Image> alienImArray;
 
-    //ship
-    int shipWidth = tileSize*2;
+    // Store in array
+    ArrayList<Image> alienImgArray; // Corrected from 'allieiimgarray'
+
+    // Ship
+    int shipWidth = tileSize * 2;
     int shipHeight = tileSize;
-    int shipX = tileSize*columns/2 - tileSize;
-    int shipY = boardWeight - tileSize*2;
+    int shipX = tileSize * columns / 2 - tileSize;
+    int shipY = boardHeight - tileSize * 2;
 
+    Block ship; // Declare the ship as a Block object
 
-    // constructor
+    // Constructor
     SpaceInvaders() {
-        setPreferredSize(new Dimension(boardWidth,boardHeight));
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
 
-        // load images
+        // Load images
         shipImg = new ImageIcon(getClass().getResource("./images/ship.png")).getImage();
-        alienImg= new ImageIcon(getClass().getResource("./images/alien.png")).getImage();
+        alienImg = new ImageIcon(getClass().getResource("./images/alien.png")).getImage();
         alienCyanImg = new ImageIcon(getClass().getResource("./images/alien-cyan.png")).getImage();
         alienMagentaImg = new ImageIcon(getClass().getResource("./images/alien-magenta.png")).getImage();
         alienYellowImg = new ImageIcon(getClass().getResource("./images/alien-yellow.png")).getImage();
 
-        alienImgArray = new ArrayList<Image>();
+        // Initialize the alien image array
+        alienImgArray = new ArrayList<>();
         alienImgArray.add(alienImg);
         alienImgArray.add(alienCyanImg);
         alienImgArray.add(alienMagentaImg);
         alienImgArray.add(alienYellowImg);
 
+        // Initialize the ship as a Block object
+        ship = new Block(shipX, shipY, shipWidth, shipHeight, shipImg);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // Draw the ship
+        g.drawImage(ship.img, ship.x, ship.y, ship.width, ship.height, null); // Fixed 'ship.shipimg' to 'ship.img'
     }
 }
